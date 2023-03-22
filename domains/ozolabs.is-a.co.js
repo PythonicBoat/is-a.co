d2305f24c273badb36d50e88ef8ebf15aef63cfd
Anas Ben Rahim <59410395+itzAnasov@users.noreply.github.com>
2023-03-22 22:44:48 +0200

    Create ozolabs.is-a.co.js

---
+++ domains/ozolabs.is-a.co.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'subdomain for my Ozolabs website',
+  domain: 'is-a.co',
+  subdomain: 'ozolabs',
+  owner: {
+    repo: 'https://github.com/ItzAnasov/ItzAnasov.github.io',
+    email: 'ozo.labs@internet.ru',
+  },
+  record: {
+    CNAME: 'anas.io.day',
+  },
+  proxy: false,
+})
