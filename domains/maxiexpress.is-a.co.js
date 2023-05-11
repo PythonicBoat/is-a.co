9f9202c0f5b769a55132e7ec9b5ce25a48214878
Vien Catigan <vien@courvix.com>
2023-05-11 17:27:13 +0800

    Create maxiexpress.is-a.co.js

---
+++ domains/maxiexpress.is-a.co.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'subdomain for my protfolio website',
+  domain: 'is-a.co',
+  subdomain: 'maxiexpress',
+  owner: {
+    repo: 'https://github.com/VienDeveloper/MaxiExpress',
+    email: 'vien@courvix.com',
+  },
+  record: {
+    TXT: ['_github-pages-challenge-VienDeveloper.maxiexpress', '4403b8872c963a2cde81d52580b001'],
+  },
+  proxy: false,
+})
